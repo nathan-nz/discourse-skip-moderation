e9a62aa1f0cea66d81aebd1d2e97d073a6840b38
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2023-12-13 18:26:47 +1300

    Update plugin.rb
    Trying something else

--- plugin.rb
+++ plugin.rb
@@ -9,27 +9,25 @@ enabled_site_setting :skip_moderation_enabled
 
 after_initialize do
   module ::DiscourseSkipModeration
-    def post_needs_approval?(manager)
-      superResult = super
-      Rails.logger.info("Skip moderation: superResult = #{superResult}")
-      return superResult if ((!(SiteSetting.skip_moderation_enabled)) || (superResult != :skip))
-
-      if SiteSetting.skip_moderation_groups_categories.is_a? String
-        groupName = manager.user.groups.pluck(:name).map(&:downcase)
+    def create_post
+      if SiteSetting.skip_moderation_enabled && SiteSetting.skip_moderation_groups_categories.is_a?(String)
+        groupName = @opts[:user].groups.pluck(:name).map(&:downcase)
         Rails.logger.info("Skip moderation: groupName = #{groupName}")
-        categoryName = manager.args[:category].downcase
+        categoryName = @opts[:category].downcase
         Rails.logger.info("Skip moderation: categoryName = #{categoryName}")
         groupCategoryArray = SiteSetting.skip_moderation_groups_categories.downcase.split("|")
         Rails.logger.info("Skip moderation: groupCategoryArray = #{groupCategoryArray}")
         groupCategoryArray.each do |groupCategory|
           group, category = groupCategory.split(":")
-          return :skip if groupName.include?(group) && category == categoryName
+          if groupName.include?(group) && category == categoryName
+            @opts[:skip_validations] = true
+            break
+          end
         end
       end
-
-      return :trust_level
+      super
     end
   end
 
-  NewPostManager.singleton_class.prepend ::DiscourseSkipModeration
+  PostCreator.prepend ::DiscourseSkipModeration
 end

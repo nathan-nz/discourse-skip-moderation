8b506b4f807ca2cc610712684d68034d8619d267
Nathan Kershaw <63566309+nathan-nz@users.noreply.github.com>
2023-12-13 18:13:57 +1300

    Update plugin.rb
    Added logging

--- plugin.rb
+++ plugin.rb
@@ -11,12 +11,16 @@ after_initialize do
   module ::DiscourseSkipModeration
     def post_needs_approval?(manager)
       superResult = super
+      Rails.logger.info("Skip moderation: superResult = #{superResult}")
       return superResult if ((!(SiteSetting.skip_moderation_enabled)) || (superResult != :skip))
 
       if SiteSetting.skip_moderation_groups_categories.is_a? String
         groupName = manager.user.groups.pluck(:name).map(&:downcase)
+        Rails.logger.info("Skip moderation: groupName = #{groupName}")
         categoryName = manager.args[:category].downcase
+        Rails.logger.info("Skip moderation: categoryName = #{categoryName}")
         groupCategoryArray = SiteSetting.skip_moderation_groups_categories.downcase.split("|")
+        Rails.logger.info("Skip moderation: groupCategoryArray = #{groupCategoryArray}")
         groupCategoryArray.each do |groupCategory|
           group, category = groupCategory.split(":")
           return :skip if groupName.include?(group) && category == categoryName
